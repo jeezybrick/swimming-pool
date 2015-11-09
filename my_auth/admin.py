e5945e0cfe254087f3c0d4660afc33afe421214b
jeezybrick <smooker14@gmail.com>
2015-11-09 14:20:59 +0200

    css-ing flash message

--- my_auth/admin.py
+++ my_auth/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from my_auth.models import MyUser
+from my_auth.models import OAuthUser
 
 # Register your models here.
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("username", "email", "created_at", 'updated')
+    list_display = ("fullname", "username", "email", 'is_auth')
 
-admin.site.register(MyUser, UserAdmin)
+admin.site.register(OAuthUser, UserAdmin)
