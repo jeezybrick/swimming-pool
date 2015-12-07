edaac3a8ffc05862b7224332299975622badaac3
jeezybrick <smooker14@gmail.com>
2015-12-07 12:22:07 +0200

    add gulp sass and livereload(nedd extension on chrome

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     uglify = require('gulp-uglify'),
     minifyCss = require('gulp-minify-css'),
-    plumber = require('gulp-plumber'),
-    autoprefixer = require('gulp-autoprefixer'); // for error handling
+    plumber = require('gulp-plumber'), // for error handling
+    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
+    livereload = require('gulp-livereload');
 
 // default
 gulp.task('default', function() {
@@ -20,9 +22,20 @@ gulp.task('autoprefix', function(){
         .pipe(gulp.dest('css'));
 });
 
+
+// watch changes of files
+gulp.task('html', function() {
+    return gulp.src('partials/*.html')
+        .pipe(gulp.dest(''))
+        .pipe(livereload());
+});
+
+
 // watch changes of files
 gulp.task('watch', function() {
-    gulp.watch('css/*.css', ['minify-css']);
+    livereload.listen();
+    gulp.watch('css/*.css', ['minify-css', 'sass']);
+    gulp.watch('partials/*.html', ['html']);
 });
 
 // compress images
@@ -44,5 +57,14 @@ gulp.task('minify-css', function() {
   return gulp.src('css/*.css')
     .pipe(plumber())
     .pipe(minifyCss({compatibility: 'ie8'}))
-    .pipe(gulp.dest('css/minified'));
+    .pipe(gulp.dest('css/minified'))
+    .pipe(livereload());
+});
+
+
+// SCSS
+gulp.task('sass', function () {
+  gulp.src('sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css/sass'));
 });
