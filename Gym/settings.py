04f21783263fd0269aed27f29d2cc3d2ecf5fed8
Andrey Statsenko <smooker14@gmail.com>
2017-07-05 12:19:49 +0300

    Update settings.py

--- Gym/settings.py
+++ Gym/settings.py
@@ -131,6 +131,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATIC_URL = '/static/'
 STATICFILES_DIRS = ('static', )
 
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
 # Default auth model
 AUTH_USER_MODEL = 'my_auth.OAuthUser'
 
