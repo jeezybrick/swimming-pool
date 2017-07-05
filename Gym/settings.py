06902f003093482b2fc7dcadaa0a94e89deec623
Andrey Statsenko <smooker14@gmail.com>
2017-07-05 12:28:42 +0300

    Update settings.py

--- Gym/settings.py
+++ Gym/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATIC_URL = '/static/'
-STATICFILES_DIRS = ('static', )
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = ('static', )
 
 # List of finder classes that know how to find static files in
 # various locations.
