6592b9891bc39e1457be7263d649833af93e13d1
Andrey Statsenko <smooker14@gmail.com>
2017-07-05 14:54:28 +0300

    Update urls.py

--- Gym/urls.py
+++ Gym/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
+from django.conf import settings
 # from social.views import GoogleLogin
 
 urlpatterns = [
