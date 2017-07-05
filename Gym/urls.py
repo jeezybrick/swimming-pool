f9edb58e75fb3c3aebabb02741abb5ba0f94316e
Andrey Statsenko <smooker14@gmail.com>
2017-07-05 14:43:37 +0300

    Update urls.py

--- Gym/urls.py
+++ Gym/urls.py
@@ -32,3 +32,6 @@ urlpatterns = [
         template_name='layout.html'
     ), name='home'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
